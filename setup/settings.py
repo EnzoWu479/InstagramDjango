9b870db7ba313e1adfa5a9c1e829096f6c5cc038
EnzoWu <enwu2014@hotmail.com>
2022-08-04 21:31:33 -0300

    allowed host updated

--- setup/settings.py
+++ setup/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['instagram-django.herokuapp.com']
+ALLOWED_HOSTS = ['instagram-django-app.herokuapp.com']
 
 
 # Application definition
